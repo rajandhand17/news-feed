f4ecf1319c38bb39fb0a9286877d5796b59c32da
Rajan Dhand <rajan@prepr.org>
2024-11-18 12:49:21 +0530

    added commands and clean-up commands

---
+++ clean-up.sh
@@ -0,0 +1,7 @@
+composer install
+php artisan cache:clear
+php artisan view:clear
+php artisan route:clear
+php artisan clear-compiled
+php artisan optimize:clear
+php artisan config:cache
---
+++ commands.sh
@@ -0,0 +1,3 @@
+php artisan app:fetch-articles-news-api
+php artisan fetch:bbc_articles
+php artisan fetch:nyt_articles
